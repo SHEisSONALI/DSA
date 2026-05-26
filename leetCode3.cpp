5be38c58f7f6d52991607ee313b1c485aff45b37
Sonali Shekhawat <sonalii.shekhawat@gmail.com>
2026-05-26 19:44:24 +0530

    Change panagram check to anagram check

--- leetCode3.cpp
+++ leetCode3.cpp
@@ -1,22 +1,27 @@
-// to check whether a string is panagram or not
+// to check whether two strings are anagram or not
 #include <iostream>
-#include <set>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-  string s;
-  getline(cin,s);
-  
-  set<char> st;
-  for(char c: s){
-      if(c>='a' && c<='z'){
-          st.insert(c);
-      }
-  }
-  if(st.size()==26){
-      cout<<"panagram";
-  }else{
-      cout<<"not a panagram";
-  }
+   string s1,s2;
+   cout<<" enter string 1";
+   cin>>s1;
+   cout<<"enter string 2";
+   cin>>s2;
+   
+   if(s1.length()!=s2.length()){
+       cout<<"Not an anagram";
+   }
+   
+  sort(s1.begin(),s1.end());
+  sort(s2.begin(),s2.end());
+ 
+ if(s1==s2){
+     cout<<"Strings are anagram";
+ }else{
+     cout<<"Strings are not anagram";
+ }
     return 0;
 }
