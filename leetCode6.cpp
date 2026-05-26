b0c6c96dd503f606a714e44c4b400198f0cb56eb
Sonali Shekhawat <sonalii.shekhawat@gmail.com>
2026-05-26 19:58:08 +0530

    tree traversal techniques

---
+++ leetCode6.cpp
@@ -0,0 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+ Node(int val ){
+     data = val;
+     left = right = NULL;
+     }
+};
+void preorder(Node* root){
+    if(root == NULL)
+    return ;
+    
+    cout<<root->data<<" ";
+    preorder(root-> left);
+    preorder(root-> right);
+}
+void inorder(Node* root){
+    if(root == NULL)
+    return;
+    
+    inorder(root-> left);
+    cout<<root->data<<" ";
+    inorder(root-> right);
+}
+void postorder(Node* root){
+    if(root == NULL)
+    return;
+    
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+ 
+int main() {
+    Node* root =new Node(1);
+    root->left =new Node(2);
+    root->right = new Node(3);
+    root->left->left= new Node(5);
+    root->left->right=new Node(4);
+    root->right->right=new Node(6);
+    
+    cout<<"inorder ";
+    inorder(root);
+    cout<<"preorder ";
+    preorder(root);
+    cout<<"postorder ";
+    postorder(root);
+
+    return 0;
+}
